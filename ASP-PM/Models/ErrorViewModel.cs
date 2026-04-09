f6c780ee830d9848043b0f0bb61cf3660c7cf274
IlyaMayskiy <teo_may@mail.ru>
2026-04-09 18:27:40 +0700

    init project

---
+++ ASP-PM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_PM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
